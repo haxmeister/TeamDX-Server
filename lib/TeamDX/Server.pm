731cc84b5702429d57fae851f52102b6b28d3741
Joshua <haxmeister@hotmail.com>
2021-08-06 01:24:34 -0500

    debug somemore

--- lib/TeamDX/Server.pm
+++ lib/TeamDX/Server.pm
@@ -181,7 +181,7 @@ sub get_user_from_handle {
         print Dumper $this_handle;
         print caller." <-- caller\n";
     }
-    print Dumper $self->{users};
+    print Dumper @{$self->{users}};
     # this handle does not belong to a user in the user list
     return $unlogged_user;
 }
