5c0b9b3ba75001d94d919a696277cbcbe2608dd5
Joshua S. Day <haxmeister@hotmail.com>
2022-07-01 23:55:16 -0500

    adding voupr crawling code

--- lib/TeamDX/Dispatcher.pm
+++ lib/TeamDX/Dispatcher.pm
@@ -2,11 +2,14 @@ package TeamDX::Dispatcher;
 
 use strict;
 use warnings;
+use HTML::TableExtract;
+use LWP::Simple;
 
 sub new {
     my ( $class, $args ) = @_;
     my $self = bless {
         'server' => $args->{'server'},
+        'table reader' => HTML::TableExtract->new(depth => 0, count => 1 ),
     }, $class;
 
     return $self;
@@ -51,4 +54,54 @@ sub logout {
     $self->{server}->log_this( $data->{name} . " has logged out" );
 }
 
+sub VVC_Request{
+    #sample data:
+    #{"vouprID":"vvc","serverAction":"VVC_Request","url":"https://voupr.spenced.com/plugin.php?name=vvc"}
+    
+    my $self   = shift;
+    my $data   = shift;
+    my $user   = shift;
+    my %response = {
+        'clientAction'    => "VVC_Update",
+        'vouprID'         => $data->{vouprID},
+        'vouprVersion'    => "",
+        'vouprLastUpdate' => '',
+        'result'          => 1,
+        'error'           => '',
+    };
+    my $content = '';
+    
+    # trap errors from external library;
+    eval {$content = get($data->{url});};
+    
+    # if no content is received for whatever reason
+    if (! $content){
+        $response{result} = 0;
+        $response{error}  = "No data was received from ".$data->{url};
+        $user->send(encode_json(\%response));
+        $self->{server}->log_this("No data was received from ".$data->{url};);
+    }
+
+    if ($content =~ m/<meta http-equiv=\"REFRESH\" content=\"0;url=404error.php">/){
+        $response{result} = 0;
+        $response{error}  = "404 error page does not exist on voupr ".$data->{url};
+        $user->send(encode_json(\%response));
+        $self->{server}->log_this("404 error page does not exist on voupr".$data->{url};);
+    }
+
+    $self->{'table reader'}->parse($content);
+
+    foreach my $ts ($self->{'table reader'}->tables) {
+        my $plugin_name    = $ts->rows->[0][1];
+        my $plugin_version = $ts->rows->[1][1];
+        my $last_release   = $ts->rows->[2][1];
+
+        $plugin_version =~ s/\s//g ;
+        $plugin_version =~ s/\(Download\)//g;
+        
+        $response{'vouprVersion'}    = $plugin_version;
+        $response{'vouprLastUpdate'} = $last_release;
+        $user->send(encode_json(\%response));
+    }
+}
 1;
